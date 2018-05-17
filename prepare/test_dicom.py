b0ab531ce59c5ba9f3d37483217fcd521e9292b0
Yang Fang <506528950@qq.com>
2018-05-17 18:47:50 +0800

    update dicom model

--- prepare/test_dicom.py
+++ prepare/test_dicom.py
@@ -1,5 +1,6 @@
 import numpy as np # linear algebra
 import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
+# import dicom
 import dicom
 import os
 import scipy.ndimage
@@ -10,7 +11,7 @@ from mpl_toolkits.mplot3d.art3d import Poly3DCollection
 
 # Some constants
 INPUT_FOLDER = '/home/yangfang/CT/sample_images/'
-INPUT_FOLDER = '/home/yangfang/CT/CT_FangYang/sample_huaxi/'
+# INPUT_FOLDER = '/home/yangfang/CT/CT_FangYang/sample_huaxi/'
 patients = os.listdir(INPUT_FOLDER)
 patients.sort()
 
